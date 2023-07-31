bd4946c8d65e58360edcb5734f5b14e8ace6296e
lawrencemsteven <lawrencemsteven@gmail.com>
2023-07-30 23:34:37 -0400

    Add std::map to BlueprintContainer

--- Vang/src/Vang/Utility/BlueprintContainer/BlueprintContainer.h
+++ Vang/src/Vang/Utility/BlueprintContainer/BlueprintContainer.h
@@ -8,78 +8,52 @@ namespace Vang {
 	public:
 		void addBlueprint(T blueprint);
 		T& getBlueprint(uint32_t blueprint_id);
-		std::optional<uint32_t> getBlueprintId(const std::string name);
+		std::optional<uint32_t> getBlueprintId(const std::string& name);
 		bool setBlueprintId(const std::string fullTechnicalName, uint32_t id);
 
 		uint32_t size() const;
 
 	private:
-		std::vector<T> all_blueprints;
-		std::vector<uint32_t> id_to_ref;
-		std::vector<std::pair<std::string, uint32_t>> sorted_names_to_ids;
+		std::vector<T> m_all_blueprints;
+		std::vector<uint32_t> m_id_to_ref;
+		std::map<std::string, uint32_t> m_sorted_names_to_ids;
 
 		bool addSortedName(std::string name, uint32_t index);
 
 		struct BinarySearchReturn {
-			uint32_t sorted_location{0};
 			uint32_t blueprint_id{0};
 			bool found{false};
 		};
-
-		inline BinarySearchReturn sortedNamesBinarySearch(const std::string& name) {
-			if (sorted_names_to_ids.size() == 0) {
-				return {};
-			}
-
-			uint32_t start = 0;
-			uint32_t end   = sorted_names_to_ids.size();
-			uint32_t mid;
-			while (start < end) {
-				mid							  = ((end - start) / 2) + start;
-				const std::string& looking_at = sorted_names_to_ids[mid].first;
-				if (name < looking_at) {
-					end = mid;
-				}
-				else if (name > looking_at) {
-					start = mid + 1;
-				}
-				else {
-					return {mid, sorted_names_to_ids[mid].second, true};
-				}
-			}
-
-			return {start, start, false};
-		}
 	};
 
 
 	template <class T>
 	inline void BlueprintContainer<T>::addBlueprint(T blueprint) {
-		const uint32_t index = all_blueprints.size();
+		const uint32_t index = m_all_blueprints.size();
 		if (!addSortedName(std::move(blueprint.getFullTechnicalName()), index)) {
 			return;
 		}
-		all_blueprints.push_back(std::move(blueprint));
-		id_to_ref.push_back(index);
+		m_all_blueprints.push_back(std::move(blueprint));
+		m_id_to_ref.push_back(index);
 	}
 
 	template <class T>
 	inline T& BlueprintContainer<T>::getBlueprint(uint32_t blueprint_id) {
-		if (blueprint_id >= id_to_ref.size()) {
+		if (blueprint_id >= m_id_to_ref.size()) {
 			VANG_FATAL("blueprint_id out of range! ID: " + std::to_string(blueprint_id) +
-					   ", SIZE: " + std::to_string(id_to_ref.size()));
+					   ", SIZE: " + std::to_string(m_id_to_ref.size()));
 		}
-		return all_blueprints[id_to_ref[blueprint_id]];
+		return m_all_blueprints[m_id_to_ref[blueprint_id]];
 	}
 
 	template <class T>
-	inline std::optional<uint32_t> BlueprintContainer<T>::getBlueprintId(const std::string name) {
-		const auto bin_search = sortedNamesBinarySearch(std::move(name));
+	inline std::optional<uint32_t> BlueprintContainer<T>::getBlueprintId(const std::string& name) {
+		const auto blueprintId = m_sorted_names_to_ids.find(name);
 
-		if (bin_search.found) {
-			return bin_search.blueprint_id;
+		if (blueprintId == m_sorted_names_to_ids.end()) {
+			return std::nullopt;
 		}
-		return std::nullopt;
+		return blueprintId->second;
 	}
 
 	template <class T>
@@ -91,45 +65,47 @@ namespace Vang {
 			return false;
 		}
 
-		const auto current_location = sortedNamesBinarySearch(std::move(fullTechnicalName));
-		if (!current_location.found) {
-			VANG_ERROR("Blueprint Is Missing From BlueprintContainer!");
+		const auto current_location = m_sorted_names_to_ids.find(fullTechnicalName);
+		if (current_location == m_sorted_names_to_ids.end()) {
+			VANG_ERROR("Blueprint Is Missing From BlueprintContainer: " + fullTechnicalName);
 			return false;
 		}
 
 		// Item already has the correct id.
-		if (current_location.blueprint_id == id) {
+		if (current_location->second == id) {
 			return true;
 		}
 
-		const auto new_location = sortedNamesBinarySearch(getBlueprint(id).getFullTechnicalName());
+		const auto new_location =
+			m_sorted_names_to_ids.find(getBlueprint(id).getFullTechnicalName());
 
-		sorted_names_to_ids[current_location.sorted_location].second = new_location.blueprint_id;
-		sorted_names_to_ids[new_location.sorted_location].second = current_location.blueprint_id;
+		uint32_t temp			 = current_location->second;
+		current_location->second = new_location->second;
+		new_location->second	 = temp;
 
-		uint32_t temp							 = id_to_ref[current_location.blueprint_id];
-		id_to_ref[current_location.blueprint_id] = id_to_ref[new_location.blueprint_id];
-		id_to_ref[new_location.blueprint_id]	 = temp;
+		temp								  = m_id_to_ref[current_location->second];
+		m_id_to_ref[current_location->second] = m_id_to_ref[new_location->second];
+		m_id_to_ref[new_location->second]	  = temp;
 
 		return true;
 	}
 
 	template <class T>
 	inline uint32_t BlueprintContainer<T>::size() const {
-		return all_blueprints.size();
+		return m_all_blueprints.size();
 	}
 
 	template <class T>
 	inline bool BlueprintContainer<T>::addSortedName(std::string name, uint32_t index) {
-		const auto bin_search = sortedNamesBinarySearch(name);
 
-		if (bin_search.found) {
+		const uint32_t size = m_sorted_names_to_ids.size();
+
+		m_sorted_names_to_ids.emplace(name, index);
+
+		if (size + 1 != m_sorted_names_to_ids.size()) {
 			VANG_FATAL("Two identical blueprint names can not exist!" + name);
 			return false;
 		}
-
-		sorted_names_to_ids.insert(sorted_names_to_ids.begin() + bin_search.sorted_location,
-								   {std::move(name), index});
 		return true;
 	}
 
