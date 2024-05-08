0193c25a736730395d98111f4922144bbc0b69eb
lawrencemsteven <lawrencemsteven@gmail.com>
2024-05-08 15:37:17 -0400

    Add Benchmarking Scenes

--- Applications/VangBenchmarks/src/VangBenchmarks.cpp
+++ Applications/VangBenchmarks/src/VangBenchmarks.cpp
@@ -1,116 +1,12 @@
 #include <Vang.h>
 
-class PlayerMovementLayer : public Vang::Utility::Layers::Layer {
-public:
-	PlayerMovementLayer()
-		: Layer{"Player Movement"} {}
+bool initializeScene() {
+	static float sceneTime{1.0f};
+	sceneTime -= Vang::Utility::Time::deltaTime();
+	return sceneTime <= 0.0f;
+}
 
-	void onUpdate() override {
-		const auto& inputCache = Vang::getInputCache();
-		auto& player		   = Vang::getPlayer();
-
-		if (inputCache.isKeyPressed(Vang::Input::KEY::LEFT_CONTROL) ||
-			inputCache.isMouseButtonPressed(Vang::Input::MOUSE::BUTTON_5)) {
-			player.setSpeed(20.0f);
-		}
-		else {
-			player.setSpeed(3.0f);
-		}
-
-		if (inputCache.isKeyPressed(Vang::Input::KEY::W)) {
-			player.moveForward(Vang::Utility::Time::deltaTime());
-		}
-		else if (inputCache.isKeyPressed(Vang::Input::KEY::S)) {
-			player.moveForward(-Vang::Utility::Time::deltaTime());
-		}
-
-		if (inputCache.isKeyPressed(Vang::Input::KEY::D)) {
-			player.moveRight(Vang::Utility::Time::deltaTime());
-		}
-		else if (inputCache.isKeyPressed(Vang::Input::KEY::A)) {
-			player.moveRight(-Vang::Utility::Time::deltaTime());
-		}
-
-		if (inputCache.isKeyPressed(Vang::Input::KEY::NUM_1)) {
-			m_blockToBuild = Vang::Voxel::Blocks::Rainbow;
-		}
-		else if (inputCache.isKeyPressed(Vang::Input::KEY::NUM_2)) {
-			m_blockToBuild = Vang::Voxel::Blocks::Glass;
-		}
-
-		if (inputCache.isKeyPressed(Vang::Input::KEY::R)) {
-			Vang::Utility::Structure::generateStructure(
-				28, 28, 456, 456, 6, Vang::Voxel::Blocks::Gray, Vang::Voxel::Blocks::Air);
-		}
-
-		if (inputCache.isKeyPressed(Vang::Input::KEY::SPACE)) {
-			player.moveUp(Vang::Utility::Time::deltaTime());
-		}
-		else if (inputCache.isKeyPressed(Vang::Input::KEY::LEFT_SHIFT)) {
-			player.moveUp(-Vang::Utility::Time::deltaTime());
-		}
-
-		if (inputCache.isMouseButtonPressed(Vang::Input::MOUSE::BUTTON_RIGHT)) {
-			if (!m_rightClickPressed) {
-				m_rightClickPressed = true;
-				auto& world			= Vang::getCurrentWorld();
-				const auto& raycast = Vang::getPlayer().getRaycastResult();
-
-				if (raycast.hit) {
-					const auto newBlockPosition = raycast.blockHitPosition - raycast.newBlockVector;
-					const auto newBlock			= world.getBlock(newBlockPosition);
-					if (newBlock != Vang::Voxel::Blocks::None) {
-						world.setBlock(newBlockPosition, m_blockToBuild);
-					}
-				}
-			}
-		}
-		else {
-			m_rightClickPressed = false;
-		}
-
-		if (inputCache.isMouseButtonPressed(Vang::Input::MOUSE::BUTTON_LEFT)) {
-			if (!m_leftClickPressed) {
-				m_leftClickPressed	= true;
-				auto& world			= Vang::getCurrentWorld();
-				const auto& raycast = Vang::getPlayer().getRaycastResult();
-
-				if (raycast.hit && raycast.blockHit != Vang::Voxel::Blocks::None) {
-					world.setBlock(raycast.blockHitPosition, Vang::Voxel::Blocks::Air);
-				}
-			}
-		}
-		else {
-			m_leftClickPressed = false;
-		}
-	}
-
-	void onEvent(Vang::Utility::Events::Event& e) override {
-		Vang::Utility::Events::EventDispatcher dispatcher{e};
-		dispatcher.dispatch<Vang::Utility::Events::MouseMovedEvent>(
-			std::bind(&PlayerMovementLayer::mouseMovedHandler, this, std::placeholders::_1));
-	}
-
-	bool mouseMovedHandler(Vang::Utility::Events::MouseMovedEvent& e) {
-		auto& playerCamera = Vang::getPlayer().getCamera();
-
-		playerCamera.mouseRotate(e.getX(), -e.getY());
-		return true;
-	}
-
-private:
-	bool m_rightClickPressed{false};
-	bool m_leftClickPressed{false};
-
-	Vang::Voxel::Blocks m_blockToBuild{Vang::Voxel::Blocks::Rainbow};
-};
-
-
-
-
-int main() {
-	Vang::getLayerStack().pushLayer(new PlayerMovementLayer());
-
+bool loadTunnelScene() {
 	auto& world = Vang::getCurrentWorld();
 
 	// Fill World
@@ -122,8 +18,8 @@ int main() {
 		}
 	}
 
-	// Set Tunnel
-	const uint32_t radius = 3;
+	// Clear Tunnel
+	const uint32_t radius = 0;
 	for (uint32_t x = 0; x < 576; x++) {
 		for (uint32_t yMod = 288 - radius; yMod <= 288 + radius; yMod++) {
 			for (uint32_t zMod = 288 - radius; zMod <= 288 + radius; zMod++) {
@@ -132,16 +28,129 @@ int main() {
 		}
 	}
 
-	Vang::getPlayer().setPosition({0.0f, 288.0f, 288.0f});
+	return true;
+}
+
+bool runTunnelScene() {
+	const float totalSceneTime{10.0f};
+	static float sceneTime{totalSceneTime};
+	const glm::vec3 startPos{0.0f, 288.0f, 288.0f};
+	const glm::vec3 endPos{575.0f, 288.0f, 288.0f};
+
+	const glm::vec3 newPos = glm::mix(endPos, startPos, sceneTime / totalSceneTime);
+
+	Vang::getPlayer().setPosition(newPos);
+
+	sceneTime -= Vang::Utility::Time::deltaTime();
+
+	return sceneTime <= 0.0f;
+}
+
+bool loadStaircaseScene() {
+	auto& world = Vang::getCurrentWorld();
+
+	// Fill World
+	for (uint32_t x = 0; x < 576; x++) {
+		for (uint32_t y = 0; y < 576; y++) {
+			for (uint32_t z = 0; z < 576; z++) {
+				world.setBlock({x, y, z}, Vang::Voxel::Blocks::Gray);
+			}
+		}
+	}
+
+	// Clear Staircase
+	for (uint32_t x = 0; x < 576; x++) {
+		for (uint32_t stepWidth = 0; stepWidth < 3; stepWidth++) {
+			for (uint32_t stepHeight = 0; stepHeight < 4; stepHeight++) {
+				const uint32_t stepX = std::clamp(x + stepWidth, 0U, 575U);
+				const uint32_t stepY = std::clamp(x + stepHeight, 0U, 575U);
+				world.setBlock({stepX, stepY, 288}, Vang::Voxel::Blocks::Air);
+			}
+		}
+	}
+
+	return true;
+}
+
+bool runStaircaseScene() {
+	const float totalSceneTime{10.0f};
+	static float sceneTime{totalSceneTime};
+	const glm::vec3 startPos{0.0f, 1.0f, 288.0f};
+	const glm::vec3 endPos{574.0f, 575.0f, 288.0f};
+
+	const glm::vec3 newPos = glm::mix(endPos, startPos, sceneTime / totalSceneTime);
+
+	Vang::getPlayer().setPosition(newPos);
+
+	sceneTime -= Vang::Utility::Time::deltaTime();
+
+	return sceneTime <= 0.0f;
+}
+
+bool loadEmptyScene() {
+	auto& world = Vang::getCurrentWorld();
+
+	// Fill World
+	for (uint32_t x = 0; x < 576; x++) {
+		for (uint32_t y = 0; y < 576; y++) {
+			for (uint32_t z = 0; z < 576; z++) {
+				world.setBlock({x, y, z}, Vang::Voxel::Blocks::Air);
+			}
+		}
+	}
+
+	// Fill Floor
+	for (uint32_t x = 0; x < 576; x++) {
+		for (uint32_t z = 0; z < 576; z++) {
+			world.setBlock({x, 0, z}, Vang::Voxel::Blocks::Gray);
+		}
+	}
+
+	return true;
+}
+
+bool runEmptyScene() {
+	const float totalSceneTime{10.0f};
+	static float sceneTime{totalSceneTime};
+	const glm::vec3 startPos{0.0f, 575.0f, 0.0f};
+	const glm::vec3 endPos{575.0f, 575.0f, 575.0f};
+
+	const glm::vec3 newPos = glm::mix(endPos, startPos, sceneTime / totalSceneTime);
+
+	Vang::getPlayer().setPosition(newPos);
+
+	sceneTime -= Vang::Utility::Time::deltaTime();
+
+	return sceneTime <= 0.0f;
+}
+
+int main() {
+	uint32_t currentScene{0};
+	uint32_t prevScene{0};
+
+	Vang::Utility::FileIO::FileWriter outFile{"./VangBenchmarks.csv"};
 
 	while (Vang::getRunning()) {
 		Vang::update();
-		// std::cout << "FPS: " << (1.0f / Vang::Utility::Time::deltaTime()) << std::endl;
 
-		Vang::Utility::FileIO::FileWriter file{"./VangBenchmarks.txt"};
+		switch (currentScene) {
+			case 0: currentScene += static_cast<uint32_t>(initializeScene()); break;
+			case 1: currentScene += static_cast<uint32_t>(loadTunnelScene()); break;
+			case 2: currentScene += static_cast<uint32_t>(runTunnelScene()); break;
+			case 3: currentScene += static_cast<uint32_t>(loadStaircaseScene()); break;
+			case 4: currentScene += static_cast<uint32_t>(runStaircaseScene()); break;
+			case 5: currentScene += static_cast<uint32_t>(loadEmptyScene()); break;
+			case 6: currentScene += static_cast<uint32_t>(runEmptyScene()); break;
+			case 7: Vang::close();
+		}
 
-		file.writeLine("Hello World!");
+		outFile.write(std::to_string(Vang::Utility::Time::deltaTime()) + ",");
 
-		Vang::close();
+		if (prevScene != currentScene) {
+			std::cout << "Switching to Scene: " << currentScene << std::endl;
+			prevScene = currentScene;
+			outFile.writeLine("");
+			outFile.write(std::to_string(currentScene) + ",");
+		}
 	}
 }
